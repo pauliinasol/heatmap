716571a9d75d769a046475d8d939885f643abb50
Pauliina Solanne <pauliinasolanne@Pauliinas-MBP.home>
2018-11-28 21:42:08 +0200

    dropdown event handling working

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,42 @@
 import React, { Component } from "react";
 import Grid from "./Grid";
+import { Dropdown } from "semantic-ui-react";
 
 class App extends Component {
+  state = {
+    selectedDepartment: "marketing"
+  };
+
+  handleChange = (event, data) => {
+    console.log(data.value);
+    this.setState({ selectedDepartment: data.value });
+  };
+
   render() {
+    const occupations = [
+      { key: "departmentTechnology", text: "Technology", value: "technology" },
+      { key: "departmentMarketing", text: "Marketing", value: "marketing" },
+      { key: "departmentHR", text: "Human Resources", value: "hr" },
+      { key: "departmentOperations", text: "Operations", value: "operations" },
+      { key: "departmentSales", text: "Sales", value: "sales" }
+    ];
+
     return (
       <div className="App">
+        <div className="wrapper">
+          <div>
+            <Dropdown
+              className="dropdown_occupation"
+              placeholder="Skills"
+              fluid
+              search
+              selection
+              options={occupations}
+              value={this.state.selectedDepartment}
+              onChange={this.handleChange}
+            />
+          </div>
+        </div>
         <div className="ui text">
           <Grid department="technology" />
           <Grid department="marketing" />
